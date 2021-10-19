5e26bbc1d9386bb06fa1522516a51b27979abd82
piajesse <me@piajesse.com>
2021-10-19 14:20:16 -0700

    Initial Commit

---
+++ Shared/accelnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  accelnetApp.swift
+//  Shared
+//
+//  Created by piajesse on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct accelnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
